46fca054091600bce3deb78490590d5961e90645
JuniorOlivk <147674092+JuniorOlivk@users.noreply.github.com>
2023-10-12 01:47:53 +0000

    Aula 11.10

---
+++ script.sql
@@ -0,0 +1,10 @@
+-- Exibir os banco de dados do servidor
+# Comentario(msql)
+/* Comentario de varias linhas(Mysql) */
+
+SHOW DATABASES;
+
+
+-- Criaçao de novo banco de dados (CREARW DATABASE)
+-- Exibir as tabelas do banco de dados(SHOW TABLE FROM)
+CREATE DATABASE Banco_de_teste;
