c0686d18d0fe68e7e60466635a85f250205ab950
JuniorOlivk <147674092+JuniorOlivk@users.noreply.github.com>
2023-11-02 01:45:34 +0000

    01.11

--- script.sql
+++ script.sql
@@ -1,3 +1,4 @@
+-- Active: 1698887926215@@127.0.0.1@3306@Banco_de_teste
 -- Exibir os banco de dados do servidor
 # Comentario(msql)
 /* Comentario de varias linhas(Mysql) */
@@ -8,3 +9,14 @@ SHOW DATABASES;
 -- Criaçao de novo banco de dados (CREARW DATABASE)
 -- Exibir as tabelas do banco de dados(SHOW TABLE FROM)
 CREATE DATABASE Banco_de_teste;
+-- Exclui o banco de dados (DROP DATABASE)
+CREATE TABLE Banco_de_teste.alunos(
+    id INT PRIMARY KEY,
+    nome VARCHAR(255)
+);
+
+SHOW TABLES FROM Banco_de_teste;
+
+SHOW FIELDS FROM Banco_de_teste.alunos;
+
+USE Banco_de_teste;
